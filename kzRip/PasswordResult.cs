a100d880e8039b482b2a57e70bd9d00bfd1eb6d1
KristalinZH <kristalinzhelezchev@gmail.com>
2024-08-29 19:35:55 +0300

    Add result try status class and function trying a password

---
+++ kzRip/PasswordResult.cs
@@ -0,0 +1,21 @@
+﻿namespace kzRip
+{
+    public class PasswordResult
+    {
+        public PasswordResult()
+        {
+            IsPasswordCorrect = false;
+            Password = string.Empty;
+            Message = string.Empty;
+        }
+        public PasswordResult(bool result, string password, string message)
+        {
+            IsPasswordCorrect = result;
+            Password = password;
+            Message = message;
+        }
+        public bool IsPasswordCorrect { get; }
+        public string Password { get; }
+        public string Message { get; }
+    }
+}
